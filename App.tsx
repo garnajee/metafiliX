64ed734ca3818e2f3920e4989a051e3e5f83d647
garnajee <62147746+garnajee@users.noreply.github.com>
2025-12-06 15:16:39 +0100

    fix: upadte pdfjs-dist lib to latest version, update code

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { ProcessedFile, WatermarkSettings } from './types';
 import { DEFAULT_SETTINGS } from './constants';
 import { watermarkImage, watermarkPdf, convertImageToPdf } from './services/watermarkService';
 import * as pdfjsLib from 'pdfjs-dist';
-pdfjsLib.GlobalWorkerOptions.workerSrc = `${import.meta.env.BASE_URL}pdf.worker.min.js`;
+pdfjsLib.GlobalWorkerOptions.workerSrc = `${import.meta.env.BASE_URL}pdf.worker.min.mjs`;
 
 import SettingsPanel from './components/SettingsPanel';
 import Dropzone from './components/Dropzone';
--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,16 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, '.'),
     }
+  },
+  build: {
+    target: 'esnext'
+  },
+  esbuild: {
+    target: 'esnext'
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      target: 'esnext'
+    }
   }
 });
